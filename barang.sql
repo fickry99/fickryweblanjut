4f9fde5bc4ea8fa1f1cdbf88d90f2e014dcf711c
fickry99 <vikydoank28@gmail.com>
2020-03-27 09:26:25 +0700

    database

---
+++ barang.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 27, 2020 at 03:25 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `barang`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_barang`
+--
+
+CREATE TABLE `data_barang` (
+  `id_barang` int(255) NOT NULL,
+  `nama_barang` varchar(255) NOT NULL,
+  `tanggal_masuk` date NOT NULL,
+  `stok` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `data_barang`
+--
+
+INSERT INTO `data_barang` (`id_barang`, `nama_barang`, `tanggal_masuk`, `stok`) VALUES
+(2, 'terigu segitiga biru', '2020-02-03', 90),
+(3, 'terigu kujang mas', '2020-02-10', 100),
+(4, 'gula gold', '2020-02-05', 50),
+(5, 'margarin amanda', '2020-02-04', 70),
+(6, 'coklat', '2020-02-01', 50);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id`, `username`, `password`) VALUES
+(1, 'dadang', '1212');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `data_barang`
+--
+ALTER TABLE `data_barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `data_barang`
+--
+ALTER TABLE `data_barang`
+  MODIFY `id_barang` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
